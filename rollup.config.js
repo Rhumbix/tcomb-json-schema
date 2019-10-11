3dba2c4ed9c2f807b39d671b477d4c4f760d442d
Liv Carman <ocarman@rhumbix.com>
2019-10-11 10:23:39 -0700

    Only output CJS with multiple entry points

--- rollup.config.js
+++ rollup.config.js
@@ -3,36 +3,18 @@ import babel from "rollup-plugin-babel"
 import pkg from "./package.json"
 
 export default [
-    // browser-friendly UMD build
     {
-        input: "src/index.js",
-        output: {
-            name: "tcombJsonSchema",
-            file: pkg.browser,
-            format: "umd"
-        },
-        plugins: [
-            resolve(), // so Rollup can find `ms`
-            babel({
-                exclude: ["node_modules/**"]
-            })
-        ]
-    },
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it"s quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify
-    // `file` and `format` for each target)
-    {
-        input: "src/index.js",
+        input: [
+            "src/index.js",
+            "src/listeners.js",
+            "src/util.js"
+        ],
         external: ["ms"],
         output: [
-            { file: pkg.main, format: "cjs" },
-            { file: pkg.module, format: "es" }
+            { dir: "dist", format: "cjs" }
         ],
         plugins: [
+            resolve(),
             babel({
                 exclude: ["node_modules/**"]
             })
